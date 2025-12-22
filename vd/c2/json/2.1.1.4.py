7034645c4c9ee3b371a0a7e158d3747623d441af
nguyenanh <lhanh@uneti.edu.vn>
2025-12-23 00:07:04 +0700

    cập nhật btvn_vd

---
+++ vd/c2/json/2.1.1.4.py
@@ -0,0 +1,14 @@
+{
+  "lop": "K18A",
+  "mon_hoc": "Python nâng cao",
+  "sinh_vien": [
+    { "ho_ten": "Nguyen Van A", "nam_sinh": 2004, "diem_tb": 8.2 },
+    { "ho_ten": "Tran Van B", "nam_sinh": 2003, "diem_tb": 7.6 }
+  ],
+  "giang_vien": {
+    "ten": "CT",
+    "email": "ct@uneti.edu"
+  },
+  "dang_mo": True, #Boon trog py khác json,trong json viết true/false laf chữ thường 
+  "ghi_chu": None # none trog py khác null trog json
+}
